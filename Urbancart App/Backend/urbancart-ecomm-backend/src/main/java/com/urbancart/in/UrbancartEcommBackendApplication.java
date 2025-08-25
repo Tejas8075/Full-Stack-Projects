e0849fb808b8f59338ba5d5523794a5841c73259
Tejas8075 <tejasmahangade61@gmail.com>
2025-08-25 16:35:41 +0530

    Dependcies and libraries added

---
+++ Urbancart App/Backend/urbancart-ecomm-backend/src/main/java/com/urbancart/in/UrbancartEcommBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urbancart.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbancartEcommBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbancartEcommBackendApplication.class, args);
+	}
+
+}
---
+++ Urbancart App/Backend/urbancart-ecomm-backend/src/test/java/com/urbancart/in/UrbancartEcommBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbancart.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbancartEcommBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
