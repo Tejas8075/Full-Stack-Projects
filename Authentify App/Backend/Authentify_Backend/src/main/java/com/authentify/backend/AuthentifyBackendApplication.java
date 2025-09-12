a70982d38a0018ce3853c70ab98baf3c94fbc40b
Tejas8075 <tejasmahangade61@gmail.com>
2025-09-12 13:19:55 +0530

    Project Setup Completed

---
+++ Authentify App/Backend/Authentify_Backend/src/main/java/com/authentify/backend/AuthentifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.authentify.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentifyBackendApplication.class, args);
+	}
+
+}
---
+++ Authentify App/Backend/Authentify_Backend/src/test/java/com/authentify/backend/AuthentifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
