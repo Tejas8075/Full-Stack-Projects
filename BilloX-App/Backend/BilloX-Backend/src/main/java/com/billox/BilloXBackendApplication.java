7f03a65c36a2ee2df81e6fd271f18b346a0934cc
Tejas8075 <tejasmahangade61@gmail.com>
2025-10-04 13:44:48 +0530

    Initial Setup completed

---
+++ BilloX-App/Backend/BilloX-Backend/src/main/java/com/billox/BilloXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.billox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilloXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilloXBackendApplication.class, args);
+	}
+
+}
