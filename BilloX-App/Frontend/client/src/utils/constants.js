b3c44aee373f3790be328fa30816a26907b97bdc
Tejas8075 <tejasmahangade61@gmail.com>
2025-10-16 02:30:22 +0530

    constants file updated

--- BilloX-App/Frontend/client/src/utils/constants.js
+++ BilloX-App/Frontend/client/src/utils/constants.js
@@ -1,3 +1,4 @@
 export const AppConstants = {
-  RAZORPAY_KEY_ID: import.meta.env.VITE_RAZORPAY_KEY_ID
+  // RAZORPAY_KEY_ID: import.meta.env.VITE_RAZORPAY_KEY_ID
+  RAZORPAY_KEY_ID: "rzp_test_RTGKQIHHrXEoI4"
 }
